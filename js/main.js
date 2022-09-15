0e2a3392b70a7021ef92e86a658b9e31a2615ac2
Antonio-Saucedo <antoniojesus@byui.edu>
2022-09-14 18:05:51 -0600

    JSON fix

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
 const URL = "https://antonio-saucedo.github.io/wdd-330/json/project-list.json";
 
-const block1 = document.getElementById("block1");
-const block2 = document.getElementById("block2");
+const displayBlock1 = document.getElementById("block1");
+const displayBlock2 = document.getElementById("block2");
 let num = 1;
 
 function buildProjectList(info) {
   let data = info.projects;
-  data.forEach((block) => {
-    block.forEach((item) => {
-      let li = document.createElement("li");
-      let a = document.createElement("a");
+  data.forEach((project) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
 
-      if (num == 1) {
-        a.setAttribute("href", `${block1.link}`);
-        a.innerHTML = `${block1.lesson}`;
-      } else {
-        a.setAttribute("href", `${block2.link}`);
-        a.innerHTML = `${block2.lesson}`;
-      }
+    a.setAttribute("href", `${project.link}`);
+    a.innerHTML = `${project.lesson}`;
 
-      li.appendChild(a);
-      if (num == 1) {
-        block1.append(li);
-      } else {
-        block2.append(li);
-      }
-      num++;
-    });
+    li.appendChild(a);
+    if (num < 6) {
+      displayBlock1.append(li);
+    } else {
+      displayBlock2.append(li);
+    }
+    num++;
   });
 }
 
