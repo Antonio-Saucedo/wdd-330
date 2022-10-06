3a1e5a6baeaeb1265434f4e1683bbc164de56288
Antonio-Saucedo <antoniojesus@byui.edu>
2022-10-05 23:10:05 -0600

    Week 4 commit

--- week4/js/game8.js
+++ week4/js/game8.js
@@ -10,38 +10,60 @@ const view = {
   question: document.getElementById("question"),
   result: document.getElementById("result"),
   info: document.getElementById("info"),
+  start: document.getElementById("start"),
+  response: document.querySelector("#response"),
   render(target, content, attributes) {
     for (const key in attributes) {
       target.setAttribute(key, attributes[key]);
     }
     target.innerHTML = content;
   },
-  start: document.getElementById("start"),
   show(element) {
     element.style.display = "block";
   },
   hide(element) {
     element.style.display = "none";
   },
+  resetForm() {
+    this.response.answer.value = "";
+    this.response.answer.focus();
+  },
+  setup() {
+    this.show(this.question);
+    this.show(this.response);
+    this.show(this.result);
+    this.hide(this.start);
+    this.render(this.score, game.score);
+    this.render(this.result, "");
+    this.render(this.info, "");
+    this.resetForm();
+  },
+  teardown() {
+    this.hide(this.question);
+    this.hide(this.response);
+    this.show(this.start);
+  },
 };
 
 const game = {
   start(quiz) {
-    view.hide(view.start);
-    this.questions = [...quiz];
     this.score = 0;
-    // main game loop
-    for (const question of this.questions) {
-      this.question = question;
-      this.ask();
-    }
-    // end of main game loop
-    this.gameOver();
+    this.questions = [...quiz];
+    view.setup();
+    this.ask();
   },
-  ask() {
-    const question = `What is ${this.question.name}'s real name?`;
-    view.render(view.question, question);
-    const response = prompt(question);
+  ask(name) {
+    if (this.questions.length > 0) {
+      this.question = this.questions.pop();
+      const question = `What is ${this.question.name}'s real name?`;
+      view.render(view.question, question);
+    } else {
+      this.gameOver();
+    }
+  },
+  check(event) {
+    event.preventDefault();
+    const response = view.response.answer.value;
     const words = response.split(" ");
 
     for (let i = 0; i < words.length; i++) {
@@ -49,29 +71,28 @@ const game = {
         words[i][0].toUpperCase() + words[i].substring(1).toLowerCase();
     }
 
-    this.check(words.join(" "));
-  },
-  check(response) {
     const answer = this.question.realName;
-    if (response === answer) {
+    if (words.join(" ") === answer) {
       view.render(view.result, "Correct!", { class: "correct" });
-      alert("Correct!");
       this.score++;
       view.render(view.score, this.score);
     } else {
       view.render(view.result, `Wrong! The correct answer was ${answer}`, {
         class: "wrong",
       });
-      alert(`Wrong! The correct answer was ${answer}`);
     }
+    view.resetForm();
+    this.ask();
   },
   gameOver() {
     view.render(
       view.info,
       `Game Over, you scored ${this.score} point${this.score !== 1 ? "s" : ""}`
     );
-    view.show(view.start);
+    view.teardown();
   },
 };
 
 view.start.addEventListener("click", () => game.start(question), false);
+view.response.addEventListener("submit", (event) => game.check(event), false);
+view.hide(view.response);
